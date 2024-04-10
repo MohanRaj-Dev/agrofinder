a2ad15fbbc935aecdd4628c04f28a1f35c528a1f
Mohan <85388246+MohanRaj-Dev@users.noreply.github.com>
2024-04-10 04:42:26 +0100

    Update settings.py

--- fertilizer/settings.py
+++ fertilizer/settings.py
@@ -84,9 +84,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'PASSWORD': 'czxYAFMOOZMpfUmxRdCLPiceQyQUwHDb',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '50160',
     }
 }
 
